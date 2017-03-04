abf67b7ac346bf10690d03af9f19a1cc43f4ac1d
Jennifer <wuchenwen@gmail.com>
2017-03-04 10:17:58 +0800

    Initial Commit

---
+++ 小費計算/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  小費計算
+//
+//  Created by Jennifer on 2017/3/4.
+//  Copyright © 2017年 Jennifer Ｗu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
